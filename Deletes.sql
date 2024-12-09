b9627944827e38a770c80ddc7634d27b55fde80f
Julia <102771898+juliacappellazzo@users.noreply.github.com>
2024-12-09 16:57:12 +0100

    Add files via upload

---
+++ Deletes.sql
@@ -0,0 +1,92 @@
+-- Gerado por Oracle SQL Developer Data Modeler 19.4.0.350.1424
+--   em:        2022-02-15 19:37:36 BRST
+--   site:      Oracle Database 11g
+--   tipo:      Oracle Database 11g
+
+DROP TABLE t_mc_bairro CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_categoria_prod CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_cidade CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_cli_fisica CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_cli_juridica CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_cliente CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_depto CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_end_cli CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_end_func CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_estado CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_funcionario CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_logradouro CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_produto CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_sgv_produto_video CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_sgv_sac CASCADE CONSTRAINTS;
+
+DROP TABLE t_mc_sgv_visualizacao_video CASCADE CONSTRAINTS;
+
+
+DROP SEQUENCE SQ_MC_CATEGORIA_PROD;
+DROP SEQUENCE SQ_MC_PRODUTO;
+DROP SEQUENCE SQ_MC_VISUALIZACAO_VIDEO;
+DROP SEQUENCE SQ_MC_SAC;
+DROP SEQUENCE SQ_MC_CLIENTE;
+DROP SEQUENCE SQ_MC_FUNCIONARIO;
+DROP SEQUENCE SQ_MC_DEPTO;
+DROP SEQUENCE SQ_MC_LOGRADOURO;
+DROP SEQUENCE SQ_MC_BAIRRO;
+DROP SEQUENCE SQ_MC_CIDADE;
+
+
+-- Relatório do Resumo do Oracle SQL Developer Data Modeler: 
+-- 
+-- CREATE TABLE                            16
+-- CREATE INDEX                             0
+-- ALTER TABLE                             36
+-- CREATE VIEW                              0
+-- ALTER VIEW                               0
+-- CREATE PACKAGE                           0
+-- CREATE PACKAGE BODY                      0
+-- CREATE PROCEDURE                         0
+-- CREATE FUNCTION                          0
+-- CREATE TRIGGER                           2
+-- ALTER TRIGGER                            0
+-- CREATE COLLECTION TYPE                   0
+-- CREATE STRUCTURED TYPE                   0
+-- CREATE STRUCTURED TYPE BODY              0
+-- CREATE CLUSTER                           0
+-- CREATE CONTEXT                           0
+-- CREATE DATABASE                          0
+-- CREATE DIMENSION                         0
+-- CREATE DIRECTORY                         0
+-- CREATE DISK GROUP                        0
+-- CREATE ROLE                              0
+-- CREATE ROLLBACK SEGMENT                  0
+-- CREATE SEQUENCE                          0
+-- CREATE MATERIALIZED VIEW                 0
+-- CREATE MATERIALIZED VIEW LOG             0
+-- CREATE SYNONYM                           0
+-- CREATE TABLESPACE                        0
+-- CREATE USER                              0
+-- 
+-- DROP TABLESPACE                          0
+-- DROP DATABASE                            0
+-- 
+-- REDACTION POLICY                         0
+-- 
+-- ORDS DROP SCHEMA                         0
+-- ORDS ENABLE SCHEMA                       0
+-- ORDS ENABLE OBJECT                       0
+-- 
+-- ERRORS                                   0
+-- WARNINGS                                 0
